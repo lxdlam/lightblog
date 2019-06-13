b40d4ef565985b69ea33af216b0536fd5303f747
Misiteli <Lizelin199808@163.com>
2019-06-13 21:09:29 +0800

    query comments

--- src/components/Comment/CommentList.vue
+++ src/components/Comment/CommentList.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="comment-list">
-    <ul>
-      <li v-for="comment in comments" v-bind:key="comment">
-        <el-divider></el-divider>
-        <CommentItem
-          :avatar_sm="comment.avatar_sm"
-          :user_nickname="comment.user_nickname"
-          :comment="comment.comment"
-          :comment_time="comment.comment_time"
-        ></CommentItem>
-      </li>
-    </ul>
+    <div class="infinite-list-wrapper">
+      <ul
+        class="list"
+        v-infinite-scroll="load"
+        infinite-scroll-disabled="disabled"
+      >
+        <li v-for="comment in comments" v-bind:key="comment">
+          <el-divider></el-divider>
+          <CommentItem
+            :avatar_sm="comment.avatar_sm"
+            :user_nickname="comment.user_nickname"
+            :comment="comment.comment"
+            :comment_time="comment.comment_time"
+          ></CommentItem>
+        </li>
+      </ul>
+      <p v-if="loading">加载中...</p>
+      <p v-if="noMore">没有更多了</p>
+    </div>
   </div>
 </template>
 
@@ -26,23 +34,49 @@ export default {
     return {
       endNum: 10,
       comments: [],
-      commentNum: 0
+      commentNum: 0,
+      loading: false
     };
   },
   methods: {
-    // 一个使用 axios 进行 http 查询的例子
-    login() {
+    loadComment(article_id, start, end) {
       const vm = this; // 如果你需要在回调函数内调用 this，请做此绑定并在回调函数内部使用 vm
-      // 下面给出了一个登录 API 的调用例子。
       this.$api.comment
-        .fetchCommentByArticle("")
-        .then(ret => {
-          vm.token = ret.data.token;
-          vm.uid = ret.data.uid;
+        .fetchCommentByArticle(article_id, start, end)
+        .then(res => {
+          vm.comments = res.data.arr;
+          vm.commentNum = res.data.sum;
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+          console.log(err.code);
+          console.log(err.msg);
+        });
+    },
+    load() {
+      this.loading = true;
+
+      this.loadComment(this.$route.params["id"], 0, this.endNum);
+      this.endNum += 10;
+      this.loading = false;
+    }
+    // 一个使用 axios 进行 http 查询的例子
+  },
+  computed: {
+    noMore() {
+      return this.count >= this.commentNum;
+    },
+    disabled() {
+      return this.loading || this.noMore;
     }
   },
+
+  mounted: function() {
+    this.loadComment(this.$route.params["id"], 0, this.endNum);
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.loadComment(this.$route.params["id"], 0, this.endNum);
+    next();
+  }
   /* created: function() {
     this.query();
   },
@@ -52,7 +86,6 @@ export default {
       this.query();
     }
   } */
-  mounted: function() {}
 };
 </script>
 
